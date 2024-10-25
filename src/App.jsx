868a55d84cd5d756512d45c4fe8c942da8ce16db
Vladislav Safronov <midisar.150@mail.ru>
2024-10-25 22:00:03 +0300

    fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import './App.css'
+
 
 function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='bg-grey3'>
+      <div className="mx-auto border-x-[10px] border-grey1 max-w-[1200px]">
+        <header className='h-screen bg-grey3 bg-scratches'>
+          <h1 className="font-rubik text-7xl">Привет! <br /> Я Макс - твой <br /> фотограф</h1>
+          <p className="font-truet text-7xl">Творю, вопреки стандартам Безусловно, сплочённость команды</p>
+        </header>
+      </div>
+    </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white1': '#FFFCF2',
+        'grey1': '#CCC5B9',
+        'grey2': '#403D39',
+        'grey3': '#252422',
+        'red1': '#D61204', 
+      },
+      backgroundImage: {
+        'scratches': "url('./assets/img/scratches.png')",
+      },
+      fontFamily: {
+        'rubik': ['Rubik'],
+        'truet': ['Truet'],
+      },
+    },
   },
   plugins: [],
 }
